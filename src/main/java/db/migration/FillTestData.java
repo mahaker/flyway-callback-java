f8a2c88a3d3c6088da4961bdce5dcbe8b2b726b4
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-05-24 21:14:49 +0900

    javaでafterMigrateを実行した

--- src/main/java/db/migration/FillTestData.java
+++ src/main/java/db/migration/FillTestData.java
@@ -1,4 +1,34 @@
 package db.migration;
 
-public class FillTestData {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.flywaydb.core.api.callback.Callback;
+import org.flywaydb.core.api.callback.Context;
+import org.flywaydb.core.api.callback.Event;
+
+public class FillTestData implements Callback {
+
+    @Override
+    public boolean supports(Event event, Context context) {
+        return Event.AFTER_MIGRATE.equals(event);
+    }
+
+    @Override
+    public boolean canHandleInTransaction(Event event, Context context) {
+        return false;
+    }
+
+    @Override
+    public void handle(Event event, Context context) {
+        try (
+            final PreparedStatement statement = context
+                .getConnection()
+                .prepareStatement("INSERT INTO PERSON(ID, NAME, AGE) VALUES (1, 'userA', 20), (2, 'userB', 22)")
+        ) {
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
